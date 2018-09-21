edd21e45d8578bbcb99d8947a0ca4895f29ab800
陈永龙 <cyl@gfire.cn>
2018-09-21 16:20:58 +0800

    pdf to jpg

---
+++ src/main/java/com/chenyl/pdf/App.java
@@ -0,0 +1,13 @@
+package com.chenyl.pdf;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        PDFUtil.pdf2Image("/media/iluvsnail/wine/work/gfire/研发部/gdp/参考文档/山东警务云计算技术白皮书-v0.91-11-292.pdf","/home/iluvsnail/pdftojpg",512);
+    }
+
+}
